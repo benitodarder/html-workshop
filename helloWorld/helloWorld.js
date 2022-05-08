f038e3339b6d55d44c916528284308127842cd5f
benitodarder <benitodarder@gmail.com>
2022-05-08 21:22:20 +0200

    Some sample pure JS examples

--- helloWorld/helloWorld.js
+++ helloWorld/helloWorld.js
@@ -1,3 +1,12 @@
 function greeting() {
    document.getElementById("pojoParagraf01").innerHTML =  document.getElementById("pojoString01").value;
 }
+
+function showHide() {
+   currentVisibility = document.getElementById("validations").style.visibility;
+   if (currentVisibility != 'visible') {
+      document.getElementById("validations").style.visibility = 'visible';
+   } else {
+      document.getElementById("validations").style.visibility = 'hidden';
+   }
+}
---
+++ javascript/javascript.js
@@ -0,0 +1,14 @@
+function whenMouseOver() {
+   document.getElementById("isMouseOver").style.visibility =  "visible";
+   document.getElementById("isMouseOut").style.visibility =  "hidden";
+}
+
+function whenMouseOut() {
+   document.getElementById("isMouseOver").style.visibility =  "hidden";
+   document.getElementById("isMouseOut").style.visibility =  "visible";
+}
+
+function whenLoad() {
+   document.getElementById("isMouseOver").style.visibility =  "hidden";
+   document.getElementById("isMouseOut").style.visibility =  "visible";
+}
